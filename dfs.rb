29677948ba2cafc5c8e8e81b3d3f70f18735bdc2
goreleaserbot <goreleaser@carlosbecker.com>
2017-09-10 18:23:46 +0900

    dfs version v0.0.5

--- dfs.rb
+++ dfs.rb
@@ -1,8 +1,8 @@
 class Dfs < Formula
   desc ""
   homepage ""
-  url "https://github.com/grapswiz/dfs/releases/download/v0.0.3/dfs_0.0.3_macOS_amd64.zip"
-  version "0.0.3"
+  url "https://github.com/grapswiz/dfs/releases/download/v0.0.5/dfs_0.0.5_macOS_amd64.zip"
+  version "0.0.5"
   sha256 "c33ee08a4b78be34fc5d440c3288ae088daa00c7f2f50e88b3007312d464d3a5"
 
   def install
