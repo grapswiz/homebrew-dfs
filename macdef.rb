11758af2da351c99428c62f991f2c23752984f62
goreleaserbot <goreleaser@carlosbecker.com>
2018-01-29 16:00:56 +0900

    macdef version v0.0.3

--- macdef.rb
+++ macdef.rb
@@ -1,9 +1,9 @@
 class Macdef < Formula
   desc "change and save defaults settings for Mac."
   homepage "https://github.com/grapswiz/macdef"
-  url "https://github.com/grapswiz/macdef/releases/download/v0.0.2/macdef_0.0.2_macOS_amd64.zip"
-  version "0.0.2"
-  sha256 "92ce1117370f43bc1b1a1da8b68bd0c6e86473759674d3c31980cbcf9a9da2e5"
+  url "https://github.com/grapswiz/macdef/releases/download/v0.0.3/macdef_0.0.3_macOS_amd64.zip"
+  version "0.0.3"
+  sha256 "44fdd7d031503470934709ba2bd8fbfeb4a02ed5e8c2a865846f72368d0035be"
 
   def install
     bin.install "macdef"
